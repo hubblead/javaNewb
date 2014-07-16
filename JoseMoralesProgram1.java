ce0efce818589ed39b625cc461889540da71a4b4
Jose Morales <josemorales@joses-air.home>
2014-07-16 19:28:08 -0400

    initial commit

---
+++ JoseMoralesProgram1.java
@@ -0,0 +1,13 @@
+/*Program 1 Assignment 10 By Jose Morales Mendizabal
+ * */
+public class JoseMoralesProgram1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
