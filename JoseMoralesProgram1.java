477dc3d568853d8318c6d4d3c25ee45d132e5dda
jfmm <moralejf@gmail.com>
2014-07-16 22:45:46 -0400

    added while loop to store quarterly rainfall data

--- JoseMoralesProgram1.java
+++ JoseMoralesProgram1.java
@@ -1,5 +1,6 @@
 /*Program 1 Assignment 10 By Jose Morales Mendizabal
  ==================================================== */
+
 import java.util.Scanner;
 
 public class JoseMoralesProgram1 {
@@ -9,7 +10,48 @@ public class JoseMoralesProgram1 {
 		// Instantiate Scanner Object
 		Scanner input = new Scanner(System.in);
 
-		System.out.println(45);
-	}
+		// declare and initialize quarter variables
+		double 	quarter1 = 0; 
+		double	quarter2 = 0;
+		double 	quarter3 = 0;
+		double 	quarter4 = 0;
+		
+		// declare and init counter
+		int counter = 0;
+		
+		while (counter < 4) {
+			
+			// up counter with each iteration
+			counter += 1;
+			System.out.println("Enter the rainfall for Quarter  " + counter + ", in inches:");
+			
+			switch(counter){
+			case 1: 
+				quarter1 = input.nextDouble();
+				break;
+				
+			case 2: 
+				quarter2 = input.nextDouble();
+				break;
+				
+			case 3: 
+				quarter3 = input.nextDouble();
+				break;
+				
+			case 4: 
+				quarter4 = input.nextDouble();
+				break;
+			
+			}
+			
+			
+		}// end while
+		
+		
+		// Average computation
+		double average = (quarter1 + quarter2 + quarter3 + quarter4) / 4;
+		System.out.println("The average rainfall for the year was " + average + " inches.");
+	
+	} // end main method
 
 }
