8c94e08878cd6b59595077853f7b29541d1dc3c9
jfmm <moralejf@gmail.com>
2014-07-17 13:22:57 -0400

    added default message for if block/cleaned up indentation

--- JoseMoralesProgram1.java
+++ JoseMoralesProgram1.java
@@ -50,33 +50,41 @@ public class JoseMoralesProgram1 {
 		
 		
 		
-			// use if block to determine which quarter had the most rain
-			if (quarter1 > quarter2 && quarter1 > quarter3 && quarter1 > quarter4){
-				
-				System.out.println("Quarter 1 had the most rainfall this year, with " + quarter1 + " inches of rain");
-				
-			} else if (quarter2 > quarter1 && quarter2 > quarter3 && quarter2 > quarter4) {
-				
-				System.out.println("Quarter 2 had the most rainfall this year, with " + quarter2 + " inches of rain");
-				
-			} else if (quarter3 > quarter1 && quarter3 > quarter2 && quarter3 > quarter4) {
-				
-				System.out.println("Quarter 3 had the most rainfall this year, with " + quarter3 + " inches of rain");
-				
-			} else if (quarter4 > quarter1 && quarter4 > quarter2 && quarter4 > quarter3) {
-				
-				System.out.println("Quarter 4 had the most rainfall this year, with " + quarter4 + " inches of rain");
-				
-			}
+		// use if block to determine which quarter had the most rain:
+		// Each quarter rainfall value is compared to each of the other quarters. If all tests return to true
+		// that is the quarter with the most rainfall
 		
-
-			// Average computation
-			double average = (quarter1 + quarter2 + quarter3 + quarter4) / 4;
-			System.out.println("The average rainfall for the year was " + average + " inches.");
+		if (quarter1 > quarter2 && quarter1 > quarter3 && quarter1 > quarter4){
 			
-			// Total Rainfall computation
-			double totalRainfall = quarter1 + quarter2 + quarter3 + quarter4;
-			System.out.println("The total rainfall for the year was " + totalRainfall + " inches.");
+			System.out.println("Quarter 1 had the most rainfall this year, with " + quarter1 + " inches of rain");
+			
+		} else if (quarter2 > quarter1 && quarter2 > quarter3 && quarter2 > quarter4) {
+			
+			System.out.println("Quarter 2 had the most rainfall this year, with " + quarter2 + " inches of rain");
+			
+		} else if (quarter3 > quarter1 && quarter3 > quarter2 && quarter3 > quarter4) {
+			
+			System.out.println("Quarter 3 had the most rainfall this year, with " + quarter3 + " inches of rain");
+			
+		} else if (quarter4 > quarter1 && quarter4 > quarter2 && quarter4 > quarter3) {
+			
+			System.out.println("Quarter 4 had the most rainfall this year, with " + quarter4 + " inches of rain");
+			
+		} else {
+			// if all values are equal default to this message
+			System.out.println("All quarters had the exact same rainfall? You sure?");
+		}
+	
+
+		// TODO: Determine which quarter had the LEAST rainfall. 
+		
+		// Average computation
+		double average = (quarter1 + quarter2 + quarter3 + quarter4) / 4;
+		System.out.println("The average rainfall for the year was " + average + " inches.");
+		
+		// Total Rainfall computation
+		double totalRainfall = quarter1 + quarter2 + quarter3 + quarter4;
+		System.out.println("The total rainfall for the year was " + totalRainfall + " inches.");
 	
 	} // end main method
 
